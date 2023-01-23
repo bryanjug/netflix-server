b0592c63cc6af5a234b097432d988c655bcf5477
Bryan Jug <bryandjug@gmail.com>
2023-01-23 15:20:43 -0800

    vercel deployment

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ limit: "5550mb", extended: true, parameterLimit: 5550000 }))
 
-const routes = require('./routes/routes.js')(app, fs);
-
 let data = {
   "new": {
     "movies": {
@@ -186,7 +184,7 @@ async function UpdateDB() {
 }
 
 async function SetTimer() {
-  setInterval(GetDataAndUpdate, 36000000);
+  setInterval(GetDataAndUpdate, 70000000);
   //for debugging
   //GetDataAndUpdate() 
   //for railway update
@@ -196,6 +194,99 @@ SetTimer();
 
 console.log("Server is working")
 
+app.get('/', (req, res) => {
+    res.send('welcome to the development api-server');
+});
+
+const dataPath = './data/db.json';
+
+// helper methods
+const readFile = (callback, returnJson = false, filePath = dataPath, encoding = 'utf8') => {
+    fs.readFile(filePath, encoding, (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        callback(returnJson ? JSON.parse(data) : data);
+    });
+};
+
+const writeFile = (fileData, callback, filePath = dataPath, encoding = 'utf8') => {
+
+    fs.writeFile(filePath, fileData, encoding, (err) => {
+        if (err) {
+            throw err;
+        }
+
+        callback();
+    });
+};
+
+// READ
+app.get('/db', (req, res) => {
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        res.send(JSON.parse(data));
+    });
+});
+
+// CREATE
+app.post('/db', (req, res) => {
+
+    readFile(data => {
+        // Note: this isn't ideal for production use. 
+        // ideally, use something like a UUID or other GUID for a unique ID value
+        const newDbId = Date.now().toString();
+
+        // add the new data
+        data[newDbId.toString()] = req.body;
+
+        writeFile(JSON.stringify(data, null, 2), () => {
+            res.status(200).send('new data added');
+        });
+    },
+        true);
+});
+
+
+// UPDATE
+app.put('/db/:id', (req, res) => {
+
+    readFile(data => {
+
+        // add the new data
+        const dbId = req.params["id"];
+        data[dbId] = req.body;
+
+        writeFile(JSON.stringify(data, null, 2), () => {
+            res.status(200).send(`DB id:${dbId} updated`);
+        });
+    },
+        true);
+});
+
+
+// DELETE
+app.delete('/db/:id', (req, res) => {
+
+    readFile(data => {
+
+        // delete the data
+        const dbId = req.params["id"];
+        delete data[dbId];
+
+        writeFile(JSON.stringify(data, null, 2), () => {
+            res.status(200).send(`DB id:${dbId} removed`);
+        });
+    },
+        true);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
+
+module.exports = app;
